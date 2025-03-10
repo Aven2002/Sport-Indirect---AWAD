113175c93a5c82fcd10780a1734060527c6dd5ba
Aven2002 <avending2002@outlook.com>
2025-03-10 14:34:22 +0800

    Added migration for store_location

---
+++ sport_indirect/database/migrations/2025_03_10_061921_create_store_location_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('store_location', function (Blueprint $table) {
+            $table->id();
+            $table->string('storeName',50);
+            $table->string('imgPath');
+            $table->string('address');
+            $table->string('phoneNo')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('store_location');
+    }
+};
