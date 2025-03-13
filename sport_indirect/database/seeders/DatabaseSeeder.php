4924edb0e60d3096c1ed72d548f9b64a3cefa733
Aven2002 <avending2002@outlook.com>
2025-03-14 00:55:18 +0800

    Config to DatabaseSeeder calls multiple Seeder concurrently

--- sport_indirect/database/seeders/DatabaseSeeder.php
+++ sport_indirect/database/seeders/DatabaseSeeder.php
@@ -13,5 +13,12 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         //User::factory(10)->create();
+
+        $this->call([
+            UserSeeder::class,
+            ProductSeeder::class,
+            SecurityQuestionSeeder::class,
+            StoreLocationSeeder::class
+        ]);
     }
 }
